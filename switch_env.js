347613c3ec82cfc49a396acd60edd9fdbe8104b2
tatsuya <tatsuya_hirota@kronos.jp>
2017-12-14 11:32:56 +0900

    update webpack

--- switch_env.js
+++ switch_env.js
@@ -1,6 +1,6 @@
 const ENV = process.env.APP_ENV || 'development'
 const fse = require('node-fs-extra')
 
+fse.copySync('./webpack.' + ENV + '.config.js', './webpack.config.js')
 fse.copySync('./src/firebase/firebase-config.' + ENV + '.js', './src/firebase/config.js')
-
 fse.copySync('./.firebase-' + ENV + '.firebaserc', './.firebaserc')
